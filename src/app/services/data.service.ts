5fbd8bfba93329a71cf02ca64102ed464376070e
Youssef Fahmy <youssef.tfs2@gmail.com>
2025-09-06 15:19:55 +0300

    Added NPCs

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import eryndor from "@data/npcs/eryndor.json"
 import branor from "@data/npcs/branor.json"
+import callaReed from "@data/npcs/calla-reed.json"
+import daraHold from "@data/npcs/dara-hold.json"
+import fenArkwright from "@data/npcs/fen-arkwright.json"
 import { NPC } from "@app/model/npc/npc.model";
 
 @Injectable({
@@ -12,6 +15,9 @@ export class DataService {
     return [
       branor,
       eryndor,
+      callaReed,
+      daraHold,
+      fenArkwright,
     ].sort((a, b) => a.name.localeCompare(b.name))
   }
 }
